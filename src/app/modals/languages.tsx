5def239a36d52fabe287892d073eb9a7c97e4564
Lucas Machiner <lucasmachiner66@gmail.com>
2025-01-19 01:37:43 -0300

    feat: criação do modal de opções de idiomas

---
+++ src/app/modals/languages.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { View } from "react-native"
+import ContainerModal from '@/components/ContainerModal';
+import { BrazilFlag, UnitedStatesFlag } from "@/assets/svgs";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import i18n from "@/i18n";
+import { useTranslation } from "react-i18next";
+import { IDataItemLang } from "@/types";
+import { RenderItemLanguage } from "@/components/RenderItemLanguage";
+
+
+export default function LanguageModal() {
+  const { t: translation } = useTranslation();
+  const [lang, setLang] = useState('');
+
+  const dataItem: Array<IDataItemLang> = [
+    { flag: <BrazilFlag height={32} width={32} />, label: translation("settings.modalLanguages.portuguese"), origin: "(BR)", lang: "pt-BR" },
+    { flag: <UnitedStatesFlag height={32} width={32} />, label: translation("settings.modalLanguages.english"), origin: "(EUA)", lang: "en-US" },
+  ];
+
+  const changeLanguage = async (lang: string) => {
+    await AsyncStorage.setItem("language", lang);
+    i18n.changeLanguage(lang);
+    setLang(lang);
+  };
+
+  useEffect(() => {
+    AsyncStorage.getItem("language")
+      .then((resp) => setLang(resp as string))
+      .catch((e) => console.log(e))
+  }, [])
+
+  return (
+    <ContainerModal title={translation("settings.modalLanguages.headerTitle")}>
+      <View style={{
+        width: "100%",
+        marginVertical: 16,
+        gap: 8,
+      }}>
+        {
+          dataItem.map((item, index) => {
+            const selected = lang === item.lang;
+            return <RenderItemLanguage
+              key={index}
+              data={item}
+              onPress={() => changeLanguage(item.lang)}
+              selected={selected}
+            />
+          })
+        }
+      </View>
+    </ContainerModal>
+  );
+}
